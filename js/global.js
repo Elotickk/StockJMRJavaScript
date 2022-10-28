758b68f3ac45c1ed23b190c80232b7c14d9145b7
elotick <gonzalorom2001@gmail.com>
2022-10-28 13:15:45 -0300

    iniciomock

---
+++ js/global.js
@@ -0,0 +1,45 @@
+/////////////////////////// GLOBAL ///////////////////////////
+
+let carrito = []
+const mainSec = document.querySelector(".SecMain");
+const contadorCarrito = document.querySelector('#contadorCarrito');
+const contItemsCarrito = document.querySelector("#contItemsCarrito");
+const carritoBody = document.querySelector('.offcanvas-body');
+const miLocalStorage = window.localStorage;
+const imagenCarrito = document.querySelector('#contenedorCarrito');
+const wrapper = document.querySelector('#wrapper');
+
+/////////////////////////// CLASES ///////////////////////////
+
+class Producto {
+    constructor(id, title, category, price, image) {
+        this.id = id;
+        this.title = title.toUpperCase();
+        this.category = category;
+        this.price = parseFloat(price);
+        this.image = image;
+    }
+}
+
+/////////////////////////// OBJETOS ///////////////////////////
+
+
+// Funciones para traer los objetos desde listaProd.json
+
+async function obtenerDatos(url) {
+    return fetch(url).then(response => response.json());
+}
+
+async function traerProductos() {
+    const Lista = await obtenerDatos('./js/mockData.json');
+    let productos = []
+    Lista.forEach(prod => {
+        productos.push(new Producto(prod.idprod, prod.title, prod.category, prod.price, prod.imagen))
+    });
+    return productos;
+}
+
+async function traerYmostrarProductos() {
+    let productos = await traerProductos()
+    mostrarProductos(productos)
+}
